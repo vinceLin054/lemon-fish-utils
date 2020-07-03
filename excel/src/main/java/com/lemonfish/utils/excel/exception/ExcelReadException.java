e8ddbe2f750bb12872912b58c9e1693233da7f3c
linwensi <wb-lws692958@alibaba-inc.com>
2020-07-03 14:27:29 +0800

    excel utils develop

---
+++ excel/src/main/java/com/lemonfish/utils/excel/exception/ExcelReadException.java
@@ -0,0 +1,17 @@
+package com.lemonfish.utils.excel.exception;
+
+/**
+ * @author linwensi
+ * @date 2020-07-03 11:13 上午
+ */
+public class ExcelReadException extends RuntimeException {
+
+    public ExcelReadException() {
+        super();
+    }
+
+    public ExcelReadException(String message) {
+        super(message);
+    }
+
+}
